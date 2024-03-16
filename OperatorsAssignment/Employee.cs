cc969c8aae6b0489349bc8ae9c5694c6da2402e3
Bendrick01 <131765016+Bendrick01@users.noreply.github.com>
2024-03-16 15:44:58 +0000

    OperatorsAssignment

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        //Setting properties of the Employee class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Constructor for the Employee class.
+        public Employee(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        // Overloading the '==' operator.
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            //
+            if (Equals(employee1, employee2)) return true;
+
+            // Handling null
+            if (employee1 is null || employee2 is null) return false;
+
+            // Comparing the ID's
+            return employee1.Id == employee2.Id;
+        }
+
+        // Overloading the '!=' operator.
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+        return !(employee1 == employee2);
+        }
+    }
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two Employee objects with the same Id.
+            Employee employee1 = new Employee(1, "Person", "One");
+            Employee employee2 = new Employee(1, "Person", "Two");
+
+            // Use the overloaded '==' operator to compare the two Employee objects.
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("emp1 and emp2 have the same ID.");
+            }
+            else
+            {
+                Console.WriteLine("emp1 and emp2 have different IDs.");
+            }
+        }
+    }
+}
